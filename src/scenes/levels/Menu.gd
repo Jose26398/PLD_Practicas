d370736d3f2bd93068e22e22eb478f56922b9e75
Jose26398 <jsg26398@gmail.com>
2020-02-27 03:27:19 +0100

    Add menu backgrounds and buttons

---
+++ src/scenes/levels/Menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+func _on_continue_pressed():
+	pass
+
+
+func _on_new_game_pressed():
+	pass
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+	pass
+
+
+func _on_controls_pressed():
+	#get_tree().change_scene("res://scenes/levels/Controls.tscn")
+	pass
