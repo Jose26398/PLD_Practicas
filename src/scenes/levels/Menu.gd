b8de9defd39c3026e47edeeffaf3deba8b33af22
Jose26398 <jsg26398@gmail.com>
2020-02-28 01:58:32 +0100

    Added player (provisional sprite)

--- src/scenes/levels/Menu.gd
+++ src/scenes/levels/Menu.gd
@@ -1,19 +1,17 @@
 extends Control
 
 
-func _on_continue_pressed():
-	pass
-
-
 func _on_new_game_pressed():
 	pass
 
 
-func _on_quit_pressed():
+func _on_load_pressed():
+	pass
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene("res://scenes/levels/Controls.tscn")
+
+
+func _on_exit_pressed() -> void:
 	get_tree().quit()
-	pass
-
-
-func _on_controls_pressed():
-	#get_tree().change_scene("res://scenes/levels/Controls.tscn")
-	pass
