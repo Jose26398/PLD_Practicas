497b6bd9bea9c80a03c34bf946cd065c9f2d697c
Jose26398 <jsg26398@gmail.com>
2020-04-06 00:01:21 +0200

    Added hitboxes and hurtboxes

--- src/scenes/characters/Enemy.gd
+++ src/scenes/characters/Enemy.gd
@@ -1,19 +1,12 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func enemy_die():
+	print("dioshngfiud")
+	var pushback_direction = (global_position).normalized()
+	#move_and_slide( pushback_direction * 5000)
+	$AnimationPlayer.play("Die")
+		
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-func _on_hurtbox_area_entered(area):
-	if area.name == "player_sword":
-		print("dioshngfiud")
-		var pushback_direction = (global_position - area.global_position).normalized()
-		move_and_slide( pushback_direction * 5000)
-		$AnimationPlayer.play("Die")
+func _on_Hurtbox_area_entered(area):
+	enemy_die()
